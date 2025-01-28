5171b3f3080c7e9ee283c0852450b02f8b92dcbc
Romik3df <michal.chamilla@vdp.sk>
2025-01-28 11:55:54 +0100

    xxx

--- listok.py
+++ listok.py
@@ -13,20 +13,30 @@ c=random. randint(1,8)
 print("Dobrý deň")
 print("Dostal/a si číslo: " + str(c)) 
 print("Napíš číslo ktoré si dostal/a a stlač enter")
-a=0
-while(True):
-    a=int(input()) 
-    if a!=c:
-        continue
-    vysledok=a**2
-    print(f"skupinové číslo= {vysledok}")
-    if vysledok>10:
-        print ("Patríš do prvej skupiny")
-        c=random. randint(50,99)
-        print(f"Dostal si poradové číslo: {c}")
-    else:
-        print("Patríš do druhej skupiny")
-        c=random. randint(1,49)
-        print(f"Dostal si poradové číslo: {c}") 
-    print("Prosím počkaj pokým zahlásia tvoje poradové číslo")
-    print("Tvoj výdajný automat")
+
+if "a=0":
+    while(True):
+        a=int(input()) 
+        if a!=c:
+            continue
+        vysledok=a**2
+        print(f"skupinové číslo= {vysledok}")
+        if vysledok>10:
+            print ("Patríš do prvej skupiny")
+            c=random. randint(50,99)
+            print(f"Dostal si poradové číslo: {c}")
+        else:
+            print("Patríš do druhej skupiny")
+            c=random. randint(1,49)
+            print(f"Dostal si poradové číslo: {c}") 
+        print("Prosím počkaj pokým zahlásia tvoje poradové číslo")
+        print("Tvoj výdajný automat")
+        
+        break
+
+else:
+    print ("Nezadal si platný znak!")
+    
+
+        
+
